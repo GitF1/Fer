e5f0c027e40e86981fcea0649bc4b1318b6ddd17
KyAnh1126 <anh261104@gmail.com>
2024-07-16 08:44:59 +0700

    Change ReviewForm.js and db.json

---
+++ src/components/ReviewForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/Review.css';
+
+export default function ReviewForm() {
+    return (
+        <div className="rating-container">
+
+        </div>
+    )
+}   
